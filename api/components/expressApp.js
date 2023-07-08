79d2000f9239afc4d885fe4db466c3284667a443
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-08 19:44:06 +0200

    minor adjustments

--- api/components/expressApp.js
+++ api/components/expressApp.js
@@ -1,18 +1,21 @@
+require('dotenv').config()
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const passport = require('passport');
+const MongoStore = require('connect-mongo')(session);
 const store = new MongoStore({ url: process.env.MONGO_URI });
+const passport = require('passport');
+const cors = require('cors');
 
 const expressApp = () => {
     const app = express();
-    
+
     app.set('trust proxy', true);
-    
+
     const server = http.createServer(app);
-    
+
     app.use(bodyParser.urlencoded({ extended: true }))
         .use(bodyParser.json())
         .use(cookieParser())
@@ -37,12 +40,12 @@ const expressApp = () => {
             origin: 'https://azerty0220pl.github.io',
             credentials: true
         }));
-    
-    server.listen(5000, () => 'Server is running on port 4000');
-    return {app: app, server: server};
+
+    server.listen(5000, () => 'Server is running on port 5000');
+    return { app: app, server: server };
 }
 
-module.exports ={
+module.exports = {
     express: expressApp,
     session: session,
     store: store
---
+++ api/components/routes.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.json({ "status": "error", "payload": "No user authenticated" });
+};
+
+const routes = (app, newUser, getChat) => {
+    app.route('/login').post(passport.authenticate('local', { failureRedirect: "/failedLogin", session: true }), (req, res) => {
+        res.json({ "status": "success", "payload": req.user });
+    }, (err, req, res) => {
+        res.json({ "status": "error", "payload": err.message });
+    });
+
+    app.route('/register').post((req, res) => {
+        newUser({ username: req.body.username, password: req.body.password }).then(x => {
+            res.json(x);
+        }).catch(err => {
+            res.json({ "status": "error", "payload": err.message });
+        });
+    });
+
+    app.route("/chat").get(ensureAuthenticated, (req, res) => {
+        getChat("general").then((chat) => {
+            res.json({ "status": "success", "payload": chat });
+        }).catch(err => {
+            res.json({ "status": "error", "payload": err });
+        });
+    });
+
+    app.route("/failedLogin").get((req, res) => {
+        res.json({ "status": "error", "payload": "Failed authentication" });
+    });
+
+    app.route('/logout').get((req, res) => {
+        req.logout();
+        res.json({ "status": "success", "payload": "logout" });
+    });
+
+    app.use((req, res, next) => {
+        res.status(404).type('text').send('Not Found');
+    });
+}
+
+module.exports = {
+    routes: routes
+}
